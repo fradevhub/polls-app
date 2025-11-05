7c17dd5877041c7be68eb9cf87f62ca11cf8e0ca
fradevhub <f.cavoto@fradev.it>
2025-11-05 19:25:11 +0100

    add react-router, react-query and base structure

---
+++ frontend/src/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "./auth/AuthContext";
+import './styles/tailwind.css';
+import './styles/index.scss';
+import App from "./App";
+
+// create a client
+const queryClient = new QueryClient();
+
+// Render
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
